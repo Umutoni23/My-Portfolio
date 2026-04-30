3c74f626531983d5d8e0a2ee899314e971537cbc
Leoncie Umutoni <umutonileoncie37@gmail.com>
2026-04-30 13:47:22 +0200

    debug some errorrs

--- my-profile/vite.config.js
+++ my-profile/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/My-Portfolio/",
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+export default defineConfig(({ command }) => ({
+  base: command === "serve" ? "/" : "/My-Portfolio/",
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+}));
